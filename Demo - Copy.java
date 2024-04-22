0796803ebcd0c08f6e3ee49f6886ca896b5b11a1
abhinandan240 <[abhawsar228@gmail.com])>
2024-04-23 02:11:10 +0530

    Second file Created

---
+++ Demo - Copy.java
@@ -0,0 +1,23 @@
+class Room{
+      int height ;
+      int width ;
+      int breadth;
+      int val;
+     Room (int h,int w,int b)
+     {
+     height=h ;
+     width =w ;
+     breadth= b;
+     }
+    int volume()
+    {
+     val=height*breadth*width;
+     return val ; 
+    }
+    public static void main(String arg[])
+    {
+    Room r= new Room(2,3,4);
+    int res = r.volume();
+    System.out.println(res);
+    }
+ }         
